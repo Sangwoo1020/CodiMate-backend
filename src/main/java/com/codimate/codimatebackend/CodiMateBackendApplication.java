fb48710695f6167c10dc2e4394092ec34981d884
Sangwoo <tkddn8828@hansung.ac.kr>
2025-10-06 16:15:45 +0900

    first commit

---
+++ src/main/java/com/codimate/codimatebackend/CodiMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codimate.codimatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiMateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodiMateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codimate/codimatebackend/CodiMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codimate.codimatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodiMateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
